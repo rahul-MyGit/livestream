4c813405fe8225f4a674f5af8ccd11117d35c920
Rahul <rahul.mymail1@gmail.com>
2024-11-23 04:11:37 +0530

    Add: skeleton and dashboard

--- src/app/(connect)/u/[name]/_component/AdminDashboard.tsx
+++ src/app/(connect)/u/[name]/_component/AdminDashboard.tsx
@@ -2,9 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { useCreaterSidebar } from "@/store/use-creater-sidebar"
-import { redirect, usePathname } from "next/navigation";
-
-import {Fullscreen, KeyRound, MessageSquare, Users} from 'lucide-react'
+import { redirect, usePathname} from "next/navigation";
+import {useSession} from "next-auth/react"
+import {Fullscreen, KeyRound, MessageSquare} from 'lucide-react'
+import NavItem from "./NavItem";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface SomethingProps {
   data: string
@@ -20,6 +22,37 @@ const AdminDashboard = ({data} : SomethingProps) => {
     redirect('/home')
   }
 
+  const pathname = usePathname();
+  const {data: session} = useSession();
+
+  const routes = [
+    {
+      label : "Stream",
+      href: `/u/${session?.user?.name}`,
+      icon: Fullscreen
+    },
+    {
+      label : "Keys",
+      href: `/u/${session?.user?.name}/keys`,
+      icon: KeyRound
+    },
+    {
+      label : "Chat",
+      href: `/u/${session?.user?.name}/chat`,
+      icon: MessageSquare
+    }  
+  ]
+
+  if(!session?.user?.name) {
+    return (
+      <ul className="space-y-2">
+        {[...Array(4)].map((_, i) => (
+          <Skeleton key={i}/>
+        ))}
+      </ul>
+    )
+  }
+
   return (
     <div>
       {showLabel && (
@@ -31,6 +64,18 @@ const AdminDashboard = ({data} : SomethingProps) => {
           </p>
         </div>
       )}
+
+      <ul>
+      {routes.map((route) =>(
+        <NavItem 
+          key={route.href}
+          label={route.label}
+          icon={route.icon}
+          href={route.href}
+          isActive= {pathname === route.href}
+        />
+      ))}
+      </ul>
     </div>
   )
 }
--- src/app/(connect)/u/[name]/_component/Sidebar.tsx
+++ src/app/(connect)/u/[name]/_component/Sidebar.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import Wrapper from './Wrapper'
 import Toogle from './Toogle'
 import AdminDashboard from './AdminDashboard'
+import { SessionProvider } from 'next-auth/react'
 
 const Sidebar = () => {
     return (
         <Wrapper>
             <Toogle />
+            <SessionProvider>
             <div className='space-y-4 pt-4 lg:pt-0'>
                 <AdminDashboard data={"Exit"}/>
             </div>
+            </SessionProvider>
         </Wrapper>
     )
 }
