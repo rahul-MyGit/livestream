2f0389f251f1614f2df01f9b719fa000f88605f0
Rahul <rahul.mymail1@gmail.com>
2024-11-25 17:58:26 +0530

    Ingress

---
+++ src/app/actions/IngressAction.ts
@@ -0,0 +1,107 @@
+'use server'
+
+import {type CreateIngressOptions, IngressAudioEncodingPreset, IngressInput, IngressVideoEncodingPreset, RoomServiceClient, TrackSource, IngressClient, IngressVideoOptions, IngressAudioOptions} from 'livekit-server-sdk'
+
+
+import prisma from '@/db'
+import { auth } from '@/lib/auth'
+import { revalidatePath } from 'next/cache';
+
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+);
+
+const ingressClient = new IngressClient(process.env.LIVEKIT_API_URL!)
+
+
+export const resetIngress = async (hostIdentity: string) => {
+    const ingresses = await ingressClient.listIngress({
+        roomName: hostIdentity
+    });
+
+    const rooms = await roomService.listRooms([hostIdentity]);
+
+    for(const room of rooms){
+        await roomService.deleteRoom(room.name)
+    }
+
+    for(const ingress of ingresses){
+        if(ingress.ingressId){
+            await ingressClient.deleteIngress(ingress.ingressId)
+        }
+    }
+}
+
+
+export const createIngress = async (ingressType:IngressInput) => {
+
+    //TODO:reset prev Ingress
+    
+    const session = await auth();
+    if(!session || !session.user){
+        throw new Error("invalid User")
+    }
+
+    const user = session.user
+
+    await resetIngress(user.id)
+
+
+    const options :CreateIngressOptions = {
+        name: user.name,
+        roomName: user.id,
+        participantName: user.name,
+        participantIdentity: user.id
+    }
+
+    if(ingressType === IngressInput.WHIP_INPUT){
+        options.enableTranscoding = true
+    } else {
+        options.video = new IngressVideoOptions({
+            source: TrackSource.CAMERA,
+            encodingOptions: {
+                case: 'preset',
+                value: IngressVideoEncodingPreset.H264_1080P_30FPS_3_LAYERS
+            },
+        });
+        options.audio = new IngressAudioOptions({
+            name: "audio",
+            source: TrackSource.SCREEN_SHARE_AUDIO,
+            encodingOptions: {
+                case: 'preset',
+                value: IngressAudioEncodingPreset.OPUS_STEREO_96KBPS
+            },
+        });
+    }
+
+    const ingress = await ingressClient.createIngress(ingressType, options)
+
+    if(!ingress || !ingress.url || !ingress.streamKey){
+        throw new Error("Failed to create ingress")
+    }
+
+    await prisma.stream.update({
+        where: {
+            userId: user.id
+        },
+        data: {
+            ingressId: ingress.ingressId,
+            serverUrl: ingress.url,
+            streamKey: ingress.streamKey
+        }
+    });
+
+    revalidatePath(`/u/${user.name}/keys`)
+    return {
+        success: true,
+        data: {
+            ingressId: ingress.ingressId,
+            name: user.name,
+            streamKey: ingress.streamKey,
+            url: ingress.url,
+            inputType: ingress.inputType
+        }
+    }
+}
--- src/components/keys/ConnectModal.tsx
+++ src/components/keys/ConnectModal.tsx
@@ -1,12 +1,35 @@
 'use client'
-import React from 'react'
+import React, { ElementRef, useRef, useState, useTransition } from 'react'
 import { Dialog, DialogClose, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from '../ui/button';
 import { AlertTriangle} from 'lucide-react';
 import {Select, SelectContent,SelectItem, SelectTrigger, SelectValue} from '@/components/ui/select'
+import { IngressInput } from 'livekit-server-sdk';
+import { createIngress } from '@/app/actions/IngressAction';
+import { toast } from 'sonner';
 
-const ConnectModal = () => {
+const RTMP = String(IngressInput.RTMP_INPUT)
+const WHIP = String(IngressInput.WHIP_INPUT)
+
+type IngressType = typeof RTMP | typeof WHIP
+
+const ConnectModal = () => { 
+
+    const [IngressType, setIngressType] = useState<IngressType>(RTMP)
+    const [isPending, startTransition] = useTransition()
+    const closeRef = useRef<ElementRef<"button">>(null);
+
+    const onSubmitHandler = () => {
+        startTransition(() => {
+            createIngress(parseInt(IngressType))
+            .then(() => {
+                toast.success("Ingress created")
+                closeRef?.current?.click();
+            })
+            .catch(() => toast.error("Error while creating ingress"))
+        })
+    }
   return (
     <Dialog>
         <DialogTrigger asChild>
@@ -18,13 +41,13 @@ const ConnectModal = () => {
             <DialogHeader>
                 <DialogTitle> Generate connection</DialogTitle>
             </DialogHeader>
-            <Select>
+            <Select disabled={isPending} value={IngressType} onValueChange={(value) => setIngressType(value)}>
                 <SelectTrigger className='w-full'>
                     <SelectValue placeholder="Ingress Type" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value='RTMP'> RTMP </SelectItem>
-                    <SelectItem value='WHIP'> WHIP </SelectItem>
+                    <SelectItem value={RTMP}> RTMP </SelectItem>
+                    <SelectItem value={WHIP}> WHIP </SelectItem>
 
                 </SelectContent>
             </Select>
@@ -36,12 +59,12 @@ const ConnectModal = () => {
                 </AlertDescription>
             </Alert>
             <div className='flex justify-between'>
-                <DialogClose>
+                <DialogClose ref={closeRef} asChild>
                     <div className='rounded-xl p-2 bg-white/10'>
                         Cancel
                     </div>
                 </DialogClose>
-                <Button onClick={() => {}} variant="secondary">
+                <Button disabled={isPending} onClick={onSubmitHandler} variant="secondary">
                     Generate
                 </Button>
             </div>
